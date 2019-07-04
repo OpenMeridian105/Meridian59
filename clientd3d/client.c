e5f5eab16e9f85bcd82eee3e5ff55ce320d223d2
skittles1 <tmx883@gmail.com>
2019-07-04 22:41:40 +0800

    Reduce client CPU usage when minimized.
    Currently PeekMessage() causes the client to have high CPU usage when
    minimized, as this call continuously runs until a message needs to be
    dispatched. Use MsgWaitForMultipleObjects with a timeout of 2ms to lower
    CPU usage.

--- clientd3d/client.c
+++ clientd3d/client.c
@@ -316,26 +316,28 @@ int PASCAL WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 	while (!bQuit)
 	{
 		MainIdle();
+      if (MsgWaitForMultipleObjects(0, NULL, 0, (DWORD)1, QS_ALLINPUT) == WAIT_OBJECT_0)
+      {
+         while (PeekMessage(&msg, NULL, 0, 0, PM_NOREMOVE))
+         {
+            if (!GetMessage(&msg, NULL, 0, 0))
+            {
+               bQuit = TRUE;
+               break;
+            }
 
-		while (PeekMessage(&msg, NULL, 0, 0, PM_NOREMOVE))
-		{
-			if (!GetMessage(&msg, NULL, 0, 0))
-			{
-				bQuit = TRUE;
-				break;
-			}
+            // Forward appropriate messages for tooltips
+            if (state == STATE_GAME)
+               TooltipForwardMessage(&msg);
 
-			// Forward appropriate messages for tooltips
-			if (state == STATE_GAME)
-				TooltipForwardMessage(&msg);
-
-			/* Handle modeless dialog messages separately */
-			if ((hCurrentDlg == NULL || !IsDialogMessage(hCurrentDlg, &msg)))
-			{
-				TranslateMessage(&msg);
-				DispatchMessage(&msg);
-			}
-		}
+            /* Handle modeless dialog messages separately */
+            if ((hCurrentDlg == NULL || !IsDialogMessage(hCurrentDlg, &msg)))
+            {
+               TranslateMessage(&msg);
+               DispatchMessage(&msg);
+            }
+         }
+      }
 	}
 
 	/* Unregister our custom classes--not good to leave them around */

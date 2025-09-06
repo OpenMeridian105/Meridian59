5fab22268cdfc79a0247cd54d21ce8e04b332530
skittles1 <tmx883@gmail.com>
2025-09-07 03:39:09 +0800

    Revert "blakserv DNS Lookup"
    This reverts commit d52bc201965c7534d1f76ae4957d17fccd70f8e0.
    
    IPv4 DNS throws errors on 106, I don't use this feature so removing
    entirely until it's fixed or I have time to look at it.

--- blakserv/async_windows.c
+++ blakserv/async_windows.c
@@ -13,70 +13,46 @@ char *maintenance_masks[MAX_MAINTENANCE_MASKS];
 int num_maintenance_masks = 0;
 char *maintenance_buffer = NULL;
 
-Bool CheckMaintenanceMask(SOCKADDR* addr, int len_addr);
+Bool CheckMaintenanceMask(SOCKADDR_IN6 *addr,int len_addr);
 
-void InitAsyncConnections(void) {
+void InitAsyncConnections(void)
+{
     WSADATA WSAData;
-    struct addrinfo hints, * res;
-    char hostname[NI_MAXHOST];
 
-    if (WSAStartup(MAKEWORD(2, 2), &WSAData) != 0) {
+    if (WSAStartup(MAKEWORD(2,2),&WSAData) != 0)
+    {
         eprintf("InitAsyncConnections can't open WinSock!\n");
         return;
     }
 
-    maintenance_buffer = (char*)malloc(strlen(ConfigStr(SOCKET_MAINTENANCE_MASK)) + 1);
-    if (maintenance_buffer == NULL) {
-        eprintf("InitAsyncConnections can't allocate maintenance buffer!\n");
-        return;
+    maintenance_buffer = (char *)malloc(strlen(ConfigStr(SOCKET_MAINTENANCE_MASK)) + 1);
+    if (maintenance_buffer == NULL)
+    {
+       eprintf("InitAsyncConnections can't allocate maintenance buffer!\n");
+       return;
     }
-    strcpy(maintenance_buffer, ConfigStr(SOCKET_MAINTENANCE_MASK));
 
-    // Parse maintenance IPs and perform DNS lookups if enabled
-    maintenance_masks[num_maintenance_masks] = strtok(maintenance_buffer, ";");
-    while (maintenance_masks[num_maintenance_masks] != NULL) {
-        if (ConfigBool(SOCKET_DNS_LOOKUP)) {
-            // Try resolving with IPv6 first
-            memset(&hints, 0, sizeof(hints));
-            hints.ai_family = AF_INET6; // Use IPv6
-            hints.ai_socktype = SOCK_STREAM;
-            if (getaddrinfo(maintenance_masks[num_maintenance_masks], NULL, &hints, &res) == 0) {
-                // Get hostname from IP if possible
-                if (getnameinfo(res->ai_addr, res->ai_addrlen, hostname, sizeof(hostname), NULL, 0, NI_NAMEREQD) == 0) {
-                    // dprintf("Maintenance mask %s resolves to hostname: %s\n", maintenance_masks[num_maintenance_masks], hostname);
-                }
-                // Get string representation of IP
-                char ipstr[INET6_ADDRSTRLEN];
-                inet_ntop(AF_INET6, &((struct sockaddr_in6*)res->ai_addr)->sin6_addr, ipstr, sizeof(ipstr));
-                dprintf("Maintenance mask %s resolves to IP: %s\n", maintenance_masks[num_maintenance_masks], ipstr);
-                freeaddrinfo(res);
-            }
-            else {
-                // Try resolving with IPv4 if IPv6 fails
-                memset(&hints, 0, sizeof(hints));
-                hints.ai_family = AF_INET; // Use IPv4
-                hints.ai_socktype = SOCK_STREAM;
-                if (getaddrinfo(maintenance_masks[num_maintenance_masks], NULL, &hints, &res) == 0) {
-                    // Get hostname from IP if possible
-                    if (getnameinfo(res->ai_addr, res->ai_addrlen, hostname, sizeof(hostname), NULL, 0, NI_NAMEREQD) == 0) {
-                        // dprintf("Maintenance mask %s resolves to hostname: %s\n", maintenance_masks[num_maintenance_masks], hostname);
-                    }
-                    // Get string representation of IP
-                    char ipstr[INET_ADDRSTRLEN];
-                    inet_ntop(AF_INET, &((struct sockaddr_in*)res->ai_addr)->sin_addr, ipstr, sizeof(ipstr));
-                    dprintf("Maintenance mask %s resolves to IP: %s\n", maintenance_masks[num_maintenance_masks], ipstr);
-                    freeaddrinfo(res);
-                }
-                else {
-                    dprintf("Could not resolve maintenance mask: %s\n", maintenance_masks[num_maintenance_masks]);
-                }
-            }
-        }
+    strcpy(maintenance_buffer,ConfigStr(SOCKET_MAINTENANCE_MASK));
+
+    // now parse out each maintenance ip
+    maintenance_masks[num_maintenance_masks] = strtok(maintenance_buffer,";");
+    while (maintenance_masks[num_maintenance_masks] != NULL)
+    {
         num_maintenance_masks++;
         if (num_maintenance_masks == MAX_MAINTENANCE_MASKS)
             break;
-        maintenance_masks[num_maintenance_masks] = strtok(NULL, ";");
+        maintenance_masks[num_maintenance_masks] = strtok(NULL,";");
     }
+
+    /*
+    {
+        int i;
+        for (i=0;i<num_maintenance_masks;i++)
+        {
+            dprintf("mask %i is [%s]\n",i,maintenance_masks[i]);
+        }
+    }
+    */
 }
 
 void ExitAsyncConnections(void)
@@ -85,83 +61,112 @@ void ExitAsyncConnections(void)
         eprintf("ExitAsyncConnections can't close WinSock!\n");
 }
 
-void AsyncSocketAccept(SOCKET sock, int event, int error, int connection_type) {
-    SOCKET new_sock;
-    SOCKADDR_IN6 acc_sin; /* Accept socket address - internet style */
-    int acc_sin_len; /* Accept socket address length */
-    SOCKADDR_IN6 peer_info;
-    int peer_len;
-    struct in6_addr peer_addr;
-    connection_node conn;
-    session_node* s;
 
-    if (event != FD_ACCEPT) {
-        eprintf("AsyncSocketAccept got non-accept %i\n", event);
-        return;
-    }
+void AsyncSocketAccept(SOCKET sock,int event,int error,int connection_type)
+{
+	SOCKET new_sock;
+	SOCKADDR_IN6 acc_sin;    /* Accept socket address - internet style */
+	int acc_sin_len;        /* Accept socket address length */
+	SOCKADDR_IN6 peer_info;
+	int peer_len;
+	struct in6_addr peer_addr;
+	connection_node conn;
+	session_node *s;
+	
+	if (event != FD_ACCEPT)
+	{
+		eprintf("AsyncSocketAccept got non-accept %i\n",event);
+		return;
+	}
+	
+	if (error != 0)
+	{
+		eprintf("AsyncSocketAccept got error %i\n",error);
+		return;
+	}
+	
+	acc_sin_len = sizeof acc_sin; 
 
-    if (error != 0) {
-        eprintf("AsyncSocketAccept got error %i\n", error);
-        return;
-    }
-
-    acc_sin_len = sizeof acc_sin;
-    new_sock = accept(sock, (struct sockaddr*)&acc_sin, &acc_sin_len);
-    if (new_sock == SOCKET_ERROR) {
-        eprintf("AcceptSocketConnections accept failed, error %i\n", GetLastError());
-        return;
-    }
-
-    peer_len = sizeof peer_info;
-    if (getpeername(new_sock, (SOCKADDR*)&peer_info, &peer_len) < 0) {
-        eprintf("AcceptSocketConnections getpeername failed error %i\n", GetLastError());
-        return;
-    }
-
-    memcpy(&peer_addr, &peer_info.sin6_addr, sizeof(struct in6_addr));
-    memcpy(&conn.addr, &peer_addr, sizeof(struct in6_addr));
-    inet_ntop(AF_INET6, &peer_addr, conn.name, sizeof(conn.name));
-
-    if (connection_type == SOCKET_MAINTENANCE_PORT) {
-        if (!CheckMaintenanceMask((SOCKADDR*)&peer_info, peer_len)) {
-            lprintf("Blocked maintenance connection from %s.\n", conn.name);
-            closesocket(new_sock);
-            return;
-        }
-    }
-    else {
-        if (!CheckBlockList(&peer_addr)) {
-            lprintf("Blocked connection from %s.\n", conn.name);
-            closesocket(new_sock);
-            return;
-        }
-    }
-
-    conn.type = CONN_SOCKET;
-    conn.socket = new_sock;
-    EnterServerLock();
-    s = CreateSession(conn);
-    if (s != NULL) {
-        StartAsyncSession(s);
-        switch (connection_type) {
-        case SOCKET_PORT:
-            InitSessionState(s, STATE_SYNCHED);
-            break;
-        case SOCKET_MAINTENANCE_PORT:
-            InitSessionState(s, STATE_MAINTENANCE);
-            break;
-        default:
-            eprintf("AcceptSocketConnections got invalid connection type %i\n", connection_type);
-        }
-        /* need to do this AFTER s->conn is set in place, because the async call writes to that mem address */
-        if (ConfigBool(SOCKET_DNS_LOOKUP)) {
-            s->conn.hLookup = StartAsyncNameLookup((char*)&peer_info, s->conn.peer_data);
-        }
-        else {
-            s->conn.hLookup = 0;
-        }
-    }
-    LeaveServerLock();
+	new_sock = accept(sock,(struct sockaddr *) &acc_sin,&acc_sin_len);
+	if (new_sock == SOCKET_ERROR) 
+	{
+		eprintf("AcceptSocketConnections accept failed, error %i\n",
+			GetLastError());
+		return;
+	}
+	
+	peer_len = sizeof peer_info;
+	if (getpeername(new_sock,(SOCKADDR *)&peer_info,&peer_len) < 0)
+	{
+		eprintf("AcceptSocketConnections getpeername failed error %i\n",
+			GetLastError());
+		return;
+	}
+	
+	memcpy(&peer_addr, &peer_info.sin6_addr, sizeof(struct in6_addr));
+	memcpy(&conn.addr, &peer_addr, sizeof(struct in6_addr));
+	inet_ntop(AF_INET6, &peer_addr, conn.name, sizeof(conn.name));
+	
+	// Took out following line to prevent log files from becoming spammed with extra lines.
+	// This line is extraneous because the outcome of the authentication is always posted to logs.
+	// lprintf("Got connection from %s to be authenticated.\n", conn.name);
+	
+	if (connection_type == SOCKET_MAINTENANCE_PORT)
+	{
+		if (!CheckMaintenanceMask(&peer_info,peer_len))
+		{
+			lprintf("Blocked maintenance connection from %s.\n", conn.name);
+			closesocket(new_sock);
+			return;
+		}
+	}
+	else
+	{
+		if (!CheckBlockList(&peer_addr))
+		{
+			lprintf("Blocked connection from %s.\n", conn.name);
+			closesocket(new_sock);
+			return;
+		}
+	}
+	
+	conn.type = CONN_SOCKET;
+	conn.socket = new_sock;
+	
+	EnterServerLock();
+	
+	s = CreateSession(conn);
+	if (s != NULL)
+	{
+		StartAsyncSession(s);
+		
+		switch (connection_type)
+		{
+		case SOCKET_PORT :
+			InitSessionState(s,STATE_SYNCHED);   
+			break;
+		case SOCKET_MAINTENANCE_PORT :
+			InitSessionState(s,STATE_MAINTENANCE);
+			break;
+		default :
+			eprintf("AcceptSocketConnections got invalid connection type %i\n",connection_type);
+		}
+		
+		/* need to do this AFTER s->conn is set in place, because the async
+		call writes to that mem address */
+		
+		if (ConfigBool(SOCKET_DNS_LOOKUP))
+		{
+			// disabled due to IPv6 right now
+			//s->conn.hLookup = StartAsyncNameLookup((char *)&peer_addr,s->conn.peer_data);
+		}
+		else
+		{
+			s->conn.hLookup = 0;
+		}
+	}
+	
+	LeaveServerLock();
 }
 
 void AsyncSocketSelect(SOCKET sock,int event,int error)
@@ -223,94 +228,39 @@ void AsyncSocketSelectUDP(SOCKET sock)
    LeaveSessionLock();
 }
 
-Bool CheckMaintenanceMask(SOCKADDR* addr, int len_addr) {
-    struct addrinfo hints, * res;
+Bool CheckMaintenanceMask(SOCKADDR_IN6 *addr,int len_addr)
+{
+    IN6_ADDR mask;
     int i;
-    char hostname[NI_MAXHOST];
-    char connecting_ip[INET6_ADDRSTRLEN];
-    int result;
+    BOOL skip;
 
-    // Convert connecting address to string for logging
-    if (addr->sa_family == AF_INET) {
-        inet_ntop(AF_INET, &((struct sockaddr_in*)addr)->sin_addr, connecting_ip, sizeof(connecting_ip));
-    }
-    else if (addr->sa_family == AF_INET6) {
-        inet_ntop(AF_INET6, &((struct sockaddr_in6*)addr)->sin6_addr, connecting_ip, sizeof(connecting_ip));
-    }
-
-    // First try to get the hostname of the connecting IP
-    result = getnameinfo(addr, len_addr, hostname, sizeof(hostname), NULL, 0, NI_NAMEREQD);
-    //dprintf("Checking maintenance mask for connection from IP: %s, Hostname: %s\n",
-        //connecting_ip,
-        //(result == 0) ? hostname : "Hostname lookup failed");
-
-    for (i = 0; i < num_maintenance_masks; i++) {
-        //dprintf("Checking against maintenance mask: %s\n", maintenance_masks[i]);
-
-        // Direct hostname comparison
-        if (result == 0 && strcmp(maintenance_masks[i], hostname) == 0) {
-            //dprintf("Match found via hostname\n");
-            return True;
+    for (i=0;i<num_maintenance_masks;i++)
+    {
+        if (inet_pton(AF_INET6, maintenance_masks[i], &mask) != 1)
+        {
+            eprintf("CheckMaintenanceMask has invalid configured mask %s\n",
+                      maintenance_masks[i]);
+            continue;
         }
 
-        // Try resolving the mask for IPv4
-        memset(&hints, 0, sizeof(hints));
-        hints.ai_family = AF_INET; // Use IPv4
-        hints.ai_socktype = SOCK_STREAM;
-        if (getaddrinfo(maintenance_masks[i], NULL, &hints, &res) == 0) {
-            char mask_ip[INET_ADDRSTRLEN];
-            inet_ntop(AF_INET, &((struct sockaddr_in*)res->ai_addr)->sin_addr, mask_ip, sizeof(mask_ip));
-            //dprintf("Comparing IPv4: Mask IP %s vs Connecting IP %s\n", mask_ip, connecting_ip);
+        /* for each byte of the mask, if it's non-zero, the client must match it */
 
-            // Compare IPv4 addresses, handling mapped IPv6 addresses
-            if (addr->sa_family == AF_INET) {
-                // Direct IPv4 comparison
-                if (memcmp(&((struct sockaddr_in*)res->ai_addr)->sin_addr,
-                    &((struct sockaddr_in*)addr)->sin_addr, sizeof(struct in_addr)) == 0) {
-                    //dprintf("Match found via IPv4 address (direct)\n");
-                    freeaddrinfo(res);
-                    return True;
-                }
+        skip = 0;
+        for (int k = 0; k < sizeof(mask.u.Byte); k++)
+        {
+            if (mask.u.Byte[k] != 0 && mask.u.Byte[k] != addr->sin6_addr.u.Byte[k])
+            {
+                // mismatch
+                skip = 1;
+                break;
             }
-            else if (addr->sa_family == AF_INET6) {
-                // Check for IPv4-mapped IPv6 address
-                struct sockaddr_in6* addr6 = (struct sockaddr_in6*)addr;
-                if (IN6_IS_ADDR_V4MAPPED(&addr6->sin6_addr)) {
-                    struct in_addr* v4_addr = (struct in_addr*)(&addr6->sin6_addr.s6_addr[12]);
-                    if (memcmp(&((struct sockaddr_in*)res->ai_addr)->sin_addr, v4_addr, sizeof(struct in_addr)) == 0) {
-                        //dprintf("Match found via IPv4-mapped IPv6 address\n");
-                        freeaddrinfo(res);
-                        return True;
-                    }
-                }
-            }
-            freeaddrinfo(res);
         }
 
-        // Try resolving the mask for IPv6
-        memset(&hints, 0, sizeof(hints));
-        hints.ai_family = AF_INET6; // Use IPv6
-        hints.ai_socktype = SOCK_STREAM;
-        if (getaddrinfo(maintenance_masks[i], NULL, &hints, &res) == 0) {
-            char mask_ip[INET6_ADDRSTRLEN];
-            inet_ntop(AF_INET6, &((struct sockaddr_in6*)res->ai_addr)->sin6_addr, mask_ip, sizeof(mask_ip));
-            //dprintf("Comparing IPv6: Mask IP %s vs Connecting IP %s\n", mask_ip, connecting_ip);
+        if (skip)
+            continue;
 
-            if (addr->sa_family == AF_INET6) {
-                struct sockaddr_in6* addr6 = (struct sockaddr_in6*)addr;
-                struct sockaddr_in6* mask6 = (struct sockaddr_in6*)res->ai_addr;
-
-                // Compare IPv6 addresses directly
-                if (memcmp(&addr6->sin6_addr, &mask6->sin6_addr, sizeof(struct in6_addr)) == 0) {
-                    //dprintf("Match found via IPv6 address\n");
-                    freeaddrinfo(res);
-                    return True;
-                }
-            }
-            freeaddrinfo(res);
-        }
+        return True;
     }
-
-    dprintf("No matching maintenance mask found\n");
     return False;
 }
+
